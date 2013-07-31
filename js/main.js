ad92ab05b2f768cfdeacbbb8f445fb412f0fd3b2
Stefan A Reinsberg <stefan@phas.ubc.ca>
2013-07-30 22:40:21 -0700

    link to absolute paths for server, css, js scripts/files/includes

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ $(function () {
     $('#fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
-        url: 'server/php/'
+        url: '/jQuery-File-Upload/server/php/'
     });
 
     // Enable iframe cross-domain access via redirect option:
