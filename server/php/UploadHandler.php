90aa4e60df8b3722fd9a3315dd543504dd02f3f4
Stefan A Reinsberg <stefan@phas.ubc.ca>
2013-07-31 17:00:25 -0700

    add logging facilty through KLogger attempt input of formData for directory upload

--- server/php/UploadHandler.php
+++ server/php/UploadHandler.php
@@ -728,7 +728,9 @@ class UploadHandler
                 }
             }
             $this->set_additional_file_properties($file);
-        }
+	}
+	$log = KLogger::instance(dirname(__FILE__).'/logs', KLogger::DEBUG);
+	$log->logInfo('Upload complete');
         return $file;
     }
 
--- server/php/index.php
+++ server/php/index.php
@@ -11,9 +11,13 @@
  */
 
 error_reporting(E_ALL | E_STRICT);
+require('KLogger.php');
 require('UploadHandler.php');
 class CustomUploadHandler extends UploadHandler {
 	protected function initialize() {
+		$log   = KLogger::instance(dirname(__FILE__).'/logs', KLogger::DEBUG);
+		$log->logInfo('REQUEST',$_REQUEST);
+
 		$subdir = empty($_REQUEST['subdir']) ? '' : $this->trim_file_name($_REQUEST['subdir']);
 		$this->options['upload_dir'] .= $subdir.'/';
 		$this->options['upload_url'] .= $subdir.'/';
